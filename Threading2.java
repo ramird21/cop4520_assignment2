38306138be07aadb88ab30deae7b78ad5603b605
ramird21 <ramirdalencour@gmail.com>
2024-02-11 16:44:55 -0500

    user input is set

--- Threading2.java
+++ Threading2.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 class Threading2
 {
     public static void main(String [] args)
     {
-        System.out.println("I am working");
+        // Problem 1: Minotaur's Birthday Party
+        int N;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please specify N (Number of guests): ");
+
+        N = sc.nextInt();
+
+        
     }
 }
