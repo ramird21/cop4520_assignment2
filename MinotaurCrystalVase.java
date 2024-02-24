2afe902b0ce379d1b3e850050c6970027f458c1b
ramird21 <ramirdalencour@gmail.com>
2024-02-23 23:48:54 -0500

    i believe Prob2 is working correctly...

--- MinotaurCrystalVase.java
+++ MinotaurCrystalVase.java
@@ -6,41 +6,82 @@ import java.util.*;
 import java.util.concurrent.Semaphore;
 
 
-
-public class MinotaurCrystalVase
+class ShowRoom
 {
+    private Semaphore SRSemaphore = new Semaphore(1);
 
-    public static int N;
-    private static Semaphore bigDude = new Semaphore(1); // Allowing one access...
+    public void enteredShowRoom(int guestID) throws InterruptedException
+    {
+        System.out.println("Guest " + guestID + " is waiting outside.");
 
+        // Taking Access Away (NOW BUSY)
+        SRSemaphore.acquire();
 
+        System.out.println("Guest " + guestID + " is in the room.");
+    }
 
-    static class Guest extends Thread
+    public void exitShowRoom(int guestID)
+    {
+        System.out.println("Guest " + guestID + " going out of the room");
+
+        // Give access back (NOW AVAILABLE)
+        SRSemaphore.release();
+    }
+}
+
+class Guest extends Thread
     {
         private int guestID;
+        private final ShowRoom showroom;
 
-        public Guest(int guestID)
+        public Guest(int guestID, ShowRoom showroom)
         {
             this.guestID = guestID;
+            this.showroom = showroom;
         }
 
         public void run()
         {
+            try 
+            {
+                showroom.enteredShowRoom(guestID);
 
+                // Mimick time spent in room...
+                sleep(1000);
+
+                showroom.exitShowRoom(guestID);
+            } catch (InterruptedException e){} // Do nothing
         }
     }
 
-
+public class MinotaurCrystalVase
+{
 
     public static void main(String [] args)
     {
         // Handling Input for number of guests
         Scanner sc = new Scanner(System.in);
         System.out.print("Please specify N (Number of guests): ");
-        N = sc.nextInt();
+        int N = sc.nextInt();
+        ShowRoom showRoom = new ShowRoom();
+
+
         // Close Scanner
         sc.close();
 
         Guest [] guests = new Guest [N];
+
+        for (int i = 0; i < N; i++)
+        {
+            guests[i] = new Guest(i + 1, showRoom);
+            guests[i].start();
+        }
+
+        // Wait for the threads to be done by joining...
+        try
+        {
+            for (Guest g : guests)
+                g.join();
+        } catch (InterruptedException e) { } // Just do nothing
     }
 }
