f6fb36b2b8d84b36f86c85b7c2df26a9dd2e122e
ramird21 <ramirdalencour@gmail.com>
2024-02-23 23:30:09 -0500

    basic PA2 Stuff

--- MinotaurCrystalVase.java
+++ MinotaurCrystalVase.java
@@ -1,17 +1,46 @@
+// Ramir Dalencour
+// COP 4520, Spring 2024
+// Prof. Juan Parra
+
+import java.util.*;
+import java.util.concurrent.Semaphore;
+
 
 
 public class MinotaurCrystalVase
 {
 
+    public static int N;
+    private static Semaphore bigDude = new Semaphore(1); // Allowing one access...
 
-    
 
 
+    static class Guest extends Thread
+    {
+        private int guestID;
+
+        public Guest(int guestID)
+        {
+            this.guestID = guestID;
+        }
+
+        public void run()
+        {
+
+        }
+    }
 
 
 
     public static void main(String [] args)
     {
+        // Handling Input for number of guests
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please specify N (Number of guests): ");
+        N = sc.nextInt();
+        // Close Scanner
+        sc.close();
 
+        Guest [] guests = new Guest [N];
     }
 }
--- README.md
+++ README.md
@@ -11,7 +11,8 @@
 - I would say that Strategy 1 and 2 have quite some similarities here. The approach I am thinking there
 - would be to use Semaphores, by allowing 1 guest in at a time. When a guest go in the room they acquire
 - and when they exit they release that semaphore and allow entrance for other guest to enter the room
-
+- For as far that I can see Strategy 3 may introduce some race conditions, I am not sure as of how to
+- work around it. Thus, I will proceed with Strategy 2 to implement this...
 
 ### Effiency:
 ### Experimental Evaluation
